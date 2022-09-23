dc2882d1e17db136e54b98a09a86d0b703ae83bb
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-23 12:12:53 -0400

    added images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,34 +5,34 @@ function About() {
     <>
       <section id="about">
         <h2 className="title">About Me</h2>
+        <div id="info">
+          <img
+            src="https://clairecashmore17.github.io/Portfolio/images/claire-profile.jpg"
+            alt="Claire Cashmore"
+          />
 
-        <img
-          src="https://clairecashmore17.github.io/Portfolio/images/claire-profile.jpg"
-          alt="Claire Cashmore"
-        />
-
-        <p id="about-me">
-          I am a recent graduate with B.S. in Computer Engineering from Embry
-          Riddle Aeronautical University. I have played soccer for 18 years
-          including 4 at a collegiate level. Being athletic is a huge part of my
-          life despite my love for sitting behind a screen.
-          <br />
-          <br />
-          I plan to attend George Washington University to attain my masters in
-          Computer Science specializing in Computer Graphics, animation, and
-          multimedia. I have a large interest in the gaming and/or graphic
-          development specifically.
-          <br />
-          <br />
-          Proficient with C/C++ but have worked with python, C#, and javascript.
-          Always looking for a new opportunity to learn and become experienced
-          in any language.
-          <br />
-          <br />
-          Experienced coursework such as Data Structures and Algorithms,
-          Software Engineering Practices, Discrete Math Structures, and
-          Microprocessors.
-        </p>
+          <p id="about-me">
+            I am a graduate with B.S. in Computer Engineering from Embry Riddle
+            Aeronautical University. I completed my studies with a 3.471 GPA
+            while participating in the schools collegiate soccer team. I hope to
+            continue the pattern of maintaining a balance between staying
+            athletic and following my dreams in the software industry.
+            <br />
+            <br />
+            After completing my undergraduate degree, I decided to pursue
+            Full-Stack development through George Washington University's 2U
+            blended program. I have since gained experience in developing a
+            full-stack application and hope to find a job to apply these skills.
+            <br />
+            <br /> I am currently seeking a role as a full-stack developer or
+            software engineer. Proficient with C/C++ but have worked with C# and
+            javascript. Always looking for a new opportunity to learn and become
+            experienced in any language. <br />
+            <br /> Experienced coursework such as Data Structures and
+            Algorithms, Software Engineering Practices, Discrete Math
+            Structures, and Microprocessors, Node.js, Handlebars, HTML and CSS.
+          </p>
+        </div>
       </section>
     </>
   );
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./index.css";
 
 function Projects() {
+  const projects = [
+    {
+      name: "project 1",
+      img_src: "",
+    },
+    {
+      name: "project 2",
+      img_src: "./Movie-Matchers.png",
+    },
+  ];
   return (
     <>
-      <h1>projects!</h1>
+      <section id="projects">
+        <h2 className="title">Projects</h2>
+        <div id="proj-collection">
+          {projects.map((project) => (
+            <div className="project card">
+              <img src={project.img_src} alt="${}" />
+              <div>{project.name}</div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
