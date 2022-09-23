dbb8b5a5094a18fd826fdacac80ef735e37ec481
Claire Cashmore <clairecashmore2017@gmail.com>
2022-09-23 08:38:51 -0400

    added folders for all components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,1 +1,29 @@
 import React from "react";
+
+import "./index.css";
+function Navbar() {
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          Claire Cashmore's Portfolio
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className={`mx-2 `}>
+            <span>About Me</span>
+          </li>
+          <li className={`mx-2 `}>
+            <span>Projects</span>
+          </li>
+          <li className={`mx-2 `}>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Projects/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <>
+      <h1>projects!</h1>
+    </>
+  );
+}
+
+export default Projects;
